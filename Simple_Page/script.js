544f0c037e52be832a81681fe30e186504fd7b71
Nikolaynn01 <nikolyanaftalan@gmail.com>
2024-08-16 21:35:04 +0400

    last corection

--- Simple_Page/script.js
+++ Simple_Page/script.js
@@ -1,5 +1,5 @@
-function addText(params) {
+function addText() {
     let name = prompt("Enter your name,please!");
     let header = document.getElementById("myH1");
-    header.textContent = header.textContent.replace("...", name);   
+    header.textContent = header.textContent.replace("guest", name);   
 }
